8c31e2ac6780f81260fe6afad3b234bf5ca4c6a2
neuervinch <npandian515@gmail.com>
2025-02-23 00:31:56 +0530

    home page created

--- freelance_it/src/App.jsx
+++ freelance_it/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import JobPost from './pages/JobPost'
+import HomePage from './pages/HomePage'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <>
     <Router>
       <Routes>
+        <Route  path='/'  element ={<HomePage/>}    />
         <Route path='/register' element = {<Register/>}     />
         <Route path='/login' element = {<Login/>}     />
         <Route  path ="/job-create"  element={<JobPost/>}      />
