be7ffd953a8e94eb610102f26e4660675801f2ac
neuervinch <npandian515@gmail.com>
2025-02-22 17:21:32 +0530

    job model coded

---
+++ backend/controller/jobCreationController.js
@@ -0,0 +1,5 @@
+
+exports.createJob = async (req ,res) =>{
+    const {}
+
+}
---
+++ backend/models/jobCreationModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const jobCreationSchema = new mongoose.Schema({
+     client :{
+        type:  mongoose.Schema.Types.ObjectId, 
+        ref: "User" ,
+        required: true
+     },
+
+     title : {
+        type: String,
+        required: true
+     },
+     description : {
+        type: String,
+        required: true
+
+     },
+
+     budget : {
+        type : Number,
+        required: true
+     },
+
+     status :{
+        type : String,
+        enums : ["open", "in progress" , "completed"],
+        default : "open"
+     },
+
+     category : {
+        type : String,
+        
+     },
+     
+     tags : [{
+        type : String,
+     }],
+      createdAt : {
+        type : Date ,
+        default : Date.now
+      },
+      updatedAt: {
+        type : Date,
+        default : Date.now
+      }
+ })
+
+ jobCreationSchema.pre("save" , function(next) {
+    this.updatedAt = Date.now();
+    next();
+
+ });
+
+ module.exports = mongoose.model("JobCreation", jobCreationSchema);
