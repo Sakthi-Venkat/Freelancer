418008b5e1f78ff9a47f7239a2130ed6199ce6c0
neuervinch <npandian515@gmail.com>
2025-02-22 12:13:04 +0530

    user model created

---
+++ backend/models/userModel.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type : String ,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password: {
+        type : String ,
+        required : true
+    },
+      role: {
+        type : String ,enum :['client' , 'freelancer'],
+         required : true
+      },
+
+      profile : {
+         bio:{
+            type : String
+
+         },
+         skills: [{
+            type : String
+         }],
+         portfolioLinks : [{
+            type : String 
+         }],
+         githubLinks : [{
+            type : String
+         }]
+      },
+
+      rating : {
+            type : Number,
+            default : 0
+      ,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }},
+    updatedAt :{
+        type : Date,
+        default : Date.now
+    },
+
+
+});
+
+ // this is a functionality where we can change something if we ned to change 
+ // live date of updation  before saving it
+
+ userSchema.pre('save',function(next) {
+    this.updatedAt = Date.now();
+    next();
+ } )
+
+ module.exports = mongoose.model('USerModel', userSchema);
